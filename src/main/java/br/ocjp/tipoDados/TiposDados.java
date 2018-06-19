d1d45a96cca04fdddd35fe44f67cae37a8c6fc0d
Willian Bueno <willianbueno@outlook.com>
2018-06-18 23:59:28 -0300

    OCJP

--- src/main/java/br/ocjp/tipoDados/TiposDados.java
+++ src/main/java/br/ocjp/tipoDados/TiposDados.java
@@ -21,22 +21,44 @@ public class TiposDados {
     private char g;
     private boolean h;
 
-
     public static void main(String ...args){
-        separador(TiposDados::variavelDeclaradaExplicitamente,"Variavel declarada localmente");
-        separador(TiposDados::variavelDeclaradaImplicitamente,"Acessando variável inicializada implicitamente");
+        new TiposDados().init();
+    }
+
+    private void init() {
+        separador(this::utilizandoVariavelDeclaradaExplicitamente,"Variavel declarada localmente");
+        separador(this::utilizandoVariavelDeclaradaImplicitamente,"Acessando variável inicializada implicitamente");
+        separador(this::resultadosPontosFlutuantes,"Possíveis resultados para pontos flutuantes");
+        separador(this::variavelInicializadaEmBlocosCondicionais,"Variáveis em blocos condicionais");
     }
 
     private static void separador(MyConsumer consumer, String titulo){
         consumer.print(titulo);
     }
-    private static void variavelDeclaradaExplicitamente() {
+
+    /**
+     * Demonstra como é uma declaração explícita e sua utilização
+     */
+    private void utilizandoVariavelDeclaradaExplicitamente() {
         int x = 10; // inicialização inicializada de forma explícita
         System.out.println(x);
     }
 
+    /**
+     * Demonstra os resultados como (Infinity, -Infinity, NaN) utilizando variáveis do tipo flutuantes
+     */
+    private void resultadosPontosFlutuantes(){
+        float a = 10;
+        float b = -10;
+        float c = 0;
+        System.out.println(a/c); //Infinity
+        System.out.println(b/c); //-Infinity
+        System.out.println(c/c); //NaN
 
-    private static void variavelDeclaradaImplicitamente() {
+        //(OBS: se o tipo fosse inteiro dividindo por ZERO, o resultado seria exception: ArithmeticException by zero)
+
+    }
+    private void utilizandoVariavelDeclaradaImplicitamente() {
         Field[] declaredFields = TiposDados.class.getDeclaredFields();
         Arrays.asList(declaredFields).stream().forEach(field -> {
             try {
@@ -45,6 +67,20 @@ public class TiposDados {
                 e1.printStackTrace();
             }
         });
+    }
+
+    /**
+     * Uma variável programada para ser inicializada dentro de um bloco condicional, pode não ser inicializada, impedindo
+     * sua utilização fora do block. segue exemplo:
+     */
+    private void variavelInicializadaEmBlocosCondicionais(){
+        int a = 10;
+        int b;
+        if(a > 0){
+            b = 20;
+            System.out.println(b); //Compilada
+        }
+        //System.out.println(b); //Erro de compilação, pois não temos garantia que b foi inicializada.
     }
 
 
